e50503dc716ca04ec53d0447641b97a2f9254bd3
Rick Booth <rixcy.rixcy@gmail.com>
2018-11-13 22:52:48 +0000

    Add initial layout files

--- src/main.js
+++ src/main.js
@@ -1,8 +1,9 @@
+import '@/assets/sass/main.scss'
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-import './registerServiceWorker'
+import '@/registerServiceWorker'
 
 Vue.config.productionTip = false
 
